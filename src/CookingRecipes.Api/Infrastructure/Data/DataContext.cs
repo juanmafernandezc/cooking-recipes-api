6960e51c915119a660c54ca9c4e86b4e4adb00cc
Juan Manuel <juanmafernandezc99@gmail.com>
2024-03-27 21:17:21 +0100

    Add database config

---
+++ src/CookingRecipes.Api/Infrastructure/Data/DataContext.cs
@@ -0,0 +1,40 @@
+﻿using CookingRecipes.Api.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace CookingRecipes.Api.Infrastructure.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<RecipeIngredient> RecipeIngredients { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<RecipeIngredient>()
+                .HasKey(ri => new { ri.RecipeID, ri.IngredientID });
+
+            modelBuilder.Entity<RecipeIngredient>()
+                .HasOne(ri => ri.Recipe)
+                .WithMany(r => r.RecipeIngredients)
+                .HasForeignKey(ri => ri.RecipeID);
+
+            modelBuilder.Entity<RecipeIngredient>()
+                .HasOne(ri => ri.Ingredient)
+                .WithMany(i => i.RecipeIngredients)
+                .HasForeignKey(ri => ri.IngredientID);
+
+            modelBuilder.Entity<Recipe>()
+                .HasOne(r => r.User)
+                .WithMany(u => u.Recipes)
+                .HasForeignKey(r => r.UserID);
+        }
+    }
+}
+
+#nullable restore
