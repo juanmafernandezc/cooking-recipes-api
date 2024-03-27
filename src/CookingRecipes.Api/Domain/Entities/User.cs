b4b1e4b389f11edd70375a68d10616537a9ea9ac
Juan Manuel <juanmafernandezc99@gmail.com>
2024-03-27 21:18:41 +0100

    Add user entity

---
+++ src/CookingRecipes.Api/Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace CookingRecipes.Api.Domain.Entities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; } = new byte[32];
+        public byte[] PasswordSalt { get; set; } = new byte[32];
+        public ICollection<Recipe>? Recipes { get; set; }
+    }
+}
