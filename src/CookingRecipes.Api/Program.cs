17739d9e0065e6ace832e122e442199bd350b02e
Juan Manuel <juanmafernandezc99@gmail.com>
2024-03-28 19:08:56 +0100

    Delete comments

--- src/CookingRecipes.Api/Program.cs
+++ src/CookingRecipes.Api/Program.cs
@@ -2,10 +2,7 @@ using CookingRecipes.Api;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -14,7 +11,6 @@ builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
