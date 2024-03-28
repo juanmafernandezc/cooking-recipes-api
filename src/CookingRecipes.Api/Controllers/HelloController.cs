bdfbdf9ca8e03501623d41c0f1a84281f13cb322
Juan Manuel <juanmafernandezc99@gmail.com>
2024-03-28 14:45:57 +0100

    Add hello controller

---
+++ src/CookingRecipes.Api/Controllers/HelloController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CookingRecipes.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Hello() => Ok("Hello!");
+    }
+}
